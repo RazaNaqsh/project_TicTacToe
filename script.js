314dfa5ac50f7897136a2c20e6d4a328136658c8
RazaNaqsh <owaisraza1704@gmail.com>
2023-01-24 23:07:46 +0530

    Can mark on board and then playerchange

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 const player1 = {
-	name: "ken",
+	name: "Light",
 	symbol: "X",
 };
+const player2 = {
+	name: "L",
+	symbol: "O",
+};
 const gameBoard = (() => {
-	const board = ["X", "X", "O", "O", "O", "X", "X", "O", "X"];
+	const board = ["X", "", "O", "O", "", "X", "", "O", ""];
 	const container = document.querySelector(".container");
 	board.forEach((item, index) => {
 		const div = document.createElement("div");
 		div.classList.add("grid-item", `data-index=${index}`);
 		div.innerHTML = item;
 		container.append(div);
+	});
+	return {
+		container,
+	};
+})();
+
+const gameFlow = (() => {
+	let activePLayer = player1;
+	gameBoard.container.childNodes.forEach((childDiv) => {
+		childDiv.addEventListener("click", (e) => {
+			if (e.target.innerHTML === "") e.target.innerHTML = activePLayer.symbol;
+			if (activePLayer === player1) activePLayer = player2;
+			else activePLayer = player1;
+		});
 	});
 })();
