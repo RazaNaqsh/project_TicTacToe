f60c83e8fb345319a3440a1231d67070bb7e05d3
RazaNaqsh <owaisraza1704@gmail.com>
2023-01-24 23:18:17 +0530

    Can add the player move to board array

--- script.js
+++ script.js
@@ -7,16 +7,18 @@ const player2 = {
 	symbol: "O",
 };
 const gameBoard = (() => {
-	const board = ["X", "", "O", "O", "", "X", "", "O", ""];
+	const board = ["", "", "O", "O", "O", "", "", "", ""];
 	const container = document.querySelector(".container");
 	board.forEach((item, index) => {
 		const div = document.createElement("div");
-		div.classList.add("grid-item", `data-index=${index}`);
+		div.classList.add("grid-item");
+		div.setAttribute("data-index", index);
 		div.innerHTML = item;
 		container.append(div);
 	});
 	return {
 		container,
+		board,
 	};
 })();
 
@@ -25,6 +27,13 @@ const gameFlow = (() => {
 	gameBoard.container.childNodes.forEach((childDiv) => {
 		childDiv.addEventListener("click", (e) => {
 			if (e.target.innerHTML === "") e.target.innerHTML = activePLayer.symbol;
+
+			let elToAdd = e.target.getAttribute("data-index");
+			console.log(elToAdd);
+
+			gameBoard.board.splice(elToAdd, 1, activePLayer.symbol);
+			console.log(gameBoard.board);
+
 			if (activePLayer === player1) activePLayer = player2;
 			else activePLayer = player1;
 		});
