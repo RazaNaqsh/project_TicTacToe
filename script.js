a0fa263106e34fc687322f0ae65c771ddd74c15e
RazaNaqsh <owaisraza1704@gmail.com>
2023-01-26 21:47:48 +0530

    Refactoring code into actual module pattern

--- script.js
+++ script.js
@@ -7,67 +7,111 @@ const player2 = {
 	symbol: "O",
 };
 
-const gameFlow = (() => {
-	const activePLayer = player1;
-	const changePlayer = () => {
-		if (gameFlow.activePLayer === player1) gameFlow.activePLayer = player2;
-		else gameFlow.activePLayer = player1;
-	};
+// const gameFlow = (() => {
+// 	const activePLayer = player1;
+// 	const changePlayer = () => {
+// 		if (gameFlow.activePLayer === player1) gameFlow.activePLayer = player2;
+// 		else gameFlow.activePLayer = player1;
+// 	};
+// 	return {
+// 		activePLayer,
+// 		changePlayer,
+// 	};
+// })();
+
+// const gameBoard = (() => {
+// 	const board = ["", "", "O", "O", "O", "", "", "", ""];
+
+// 	const container = document.querySelector(".container");
+// 	board.forEach((item, index) => {
+// 		const div = document.createElement("div");
+// 		div.classList.add("grid-item");
+// 		div.setAttribute("data-index", index);
+// 		div.innerHTML = item;
+// 		container.append(div);
+// 	});
+
+// 	const childDivs = document.querySelectorAll(".grid-item");
+
+// 	childDivs.forEach((div) => {
+// 		div.addEventListener("click", play);
+// 	});
+
+// 	function play(e) {
+// 		if (e.target.innerHTML === "") {
+// 			e.target.innerHTML = gameFlow.activePLayer.symbol;
+
+// 			updateBoard(e);
+// 			checkWin();
+// 			gameFlow.changePlayer();
+// 		}
+// 	}
+
+// 	const updateBoard = (e) => {
+// 		const elToAdd = e.target.getAttribute("data-index");
+// 		board.splice(elToAdd, 1, gameFlow.activePLayer.symbol);
+// 		console.log(board);
+// 	};
+
+// 	const checkWin = () => {
+// 		if (
+// 			(board[0] === board[1] && board[0] === board[2] && board[0] !== "") ||
+// 			(board[0] === board[3] && board[0] === board[6] && board[0] !== "") ||
+// 			(board[0] === board[4] && board[0] === board[8] && board[0] !== "") ||
+// 			(board[1] === board[4] && board[1] === board[7] && board[1] !== "") ||
+// 			(board[2] === board[5] && board[2] === board[8] && board[2] !== "") ||
+// 			(board[2] === board[4] && board[2] === board[6] && board[2] !== "") ||
+// 			(board[3] === board[4] && board[3] === board[5] && board[3] !== "") ||
+// 			(board[6] === board[7] && board[6] === board[8] && board[6] !== "")
+// 		) {
+// 			console.log(`${gameFlow.activePLayer.symbol} wins`);
+// 			childDivs.forEach((div) => {
+// 				div.removeEventListener("click", play);
+// 			});
+// 		}
+// 	};
+// })();
+
+// display controller
+
+const gameBoard = (() => {
+	const boardArray = ["X", "", "", "", "", "", "", "", ""];
 	return {
-		activePLayer,
-		changePlayer,
+		boardArray,
 	};
 })();
 
-const gameBoard = (() => {
-	const board = ["", "", "O", "O", "O", "", "", "", ""];
-
-	const container = document.querySelector(".container");
-	board.forEach((item, index) => {
-		const div = document.createElement("div");
-		div.classList.add("grid-item");
-		div.setAttribute("data-index", index);
-		div.innerHTML = item;
-		container.append(div);
-	});
-
-	const childDivs = document.querySelectorAll(".grid-item");
-
-	childDivs.forEach((div) => {
-		div.addEventListener("click", play);
-	});
-
-	function play(e) {
-		if (e.target.innerHTML === "") {
-			e.target.innerHTML = gameFlow.activePLayer.symbol;
-
-			updateBoard(e);
-			checkWin();
-			gameFlow.changePlayer();
-		}
-	}
-
+const gameFlow = (() => {
+	let activePLayer = player1;
 	const updateBoard = (e) => {
 		const elToAdd = e.target.getAttribute("data-index");
-		board.splice(elToAdd, 1, gameFlow.activePLayer.symbol);
-		console.log(board);
+		gameBoard.boardArray.splice(elToAdd, 1, gameFlow.activePLayer.symbol);
 	};
-
-	const checkWin = () => {
-		if (
-			(board[0] === board[1] && board[0] === board[2] && board[0] !== "") ||
-			(board[0] === board[3] && board[0] === board[6] && board[0] !== "") ||
-			(board[0] === board[4] && board[0] === board[8] && board[0] !== "") ||
-			(board[1] === board[4] && board[1] === board[7] && board[1] !== "") ||
-			(board[2] === board[5] && board[2] === board[8] && board[2] !== "") ||
-			(board[2] === board[4] && board[2] === board[6] && board[2] !== "") ||
-			(board[3] === board[4] && board[3] === board[5] && board[3] !== "") ||
-			(board[6] === board[7] && board[6] === board[8] && board[6] !== "")
-		) {
-			console.log(`${gameFlow.activePLayer.symbol} wins`);
-			childDivs.forEach((div) => {
-				div.removeEventListener("click", play);
-			});
+	const switchPlayer = () => {
+		// if (gameFlow.activePLayer === player1) gameFlow.activePLayer = player2;
+		// else gameFlow.activePLayer = player1;
+		console.log(activePLayer);
+		if (activePLayer === player1) activePLayer = player2;
+		else activePLayer = player1;
+	};
+	const play = (e) => {
+		if (e.target.innerHTML === "") {
+			e.target.innerHTML = activePLayer.symbol;
+			updateBoard(e);
+			switchPlayer();
 		}
 	};
+
+	return {
+		activePLayer,
+		play,
+	};
+})();
+
+const displayController = (() => {
+	const grid = document.querySelectorAll(".grid-item");
+	grid.forEach((gridItem, index) => {
+		gridItem.innerHTML = gameBoard.boardArray[index];
+		gridItem.addEventListener("click", gameFlow.play);
+	});
 })();
